d87d1f3083feb5a631aa89344e411a8d0429272a
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2021-11-15 11:17:21 +0100

    Försökte lägga till mer på listan

--- src/github.java
+++ src/github.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
+import java.util.Arrays;
 
 public class github {
     public static void main(String[] args)
     {
         String antal = "";
+        int summa = 0;
         antal = JOptionPane.showInputDialog(null, "Hur många tal?");
         int antalInt = Integer.parseInt(antal);
 
         int[] lista = new int[antalInt];
-
+        for(int i = 1; i <= lista.length; i++)
+        {
+            antal = JOptionPane.showInputDialog(null, "Ange de " + i + "e numret");
+            lista[i] = Integer.parseInt(antal);
+        }
     }
 }
