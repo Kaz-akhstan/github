281f2b41f9838c3bcfbc80fcaefacc8955daf150
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2021-11-15 10:53:21 +0100

    Fixade listan

---
+++ src/github.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class github {
+    public static void main(String[] args)
+    {
+        String antal = "";
+        antal = JOptionPane.showInputDialog(null, "Hur många tal?");
+        int antalInt = Integer.parseInt(antal);
+
+        int[] lista = new int[antalInt];
+
+    }
+}
