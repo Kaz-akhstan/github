4872e70257dcb0435c5b7b6de87f051415bd4d9d
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2021-11-28 01:34:03 +0100

    Sak

---
+++ src/Coordinate_System.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class Coordinate_System {
+    public static void main(String[] args)
+    {
+        char[] cordX = new char[10];
+        Scanner in = new Scanner(System.in);
+        int cordY = 10;
+
+        int playerX = 0;
+        int playerY = 0;
+
+        for(int i = 0; i < cordY; i++)
+        {
+            if(playerY == i)
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                    cordX[index] = '-';
+                }
+                cordX[playerX] = '#';
+                System.out.println(cordX);
+            }
+            else
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                    cordX[index] = '-';
+                }
+                System.out.println(cordX);
+            }
+        }
+
+        while (true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                cordX[i] = '-';
+            }
+            String s = in.nextLine();
+            if(s.equals("d"))
+            {
+                playerX++;
+                for(int i = 0; i < cordY; i++)
+                {
+                    if(playerY == i)
+                    {
+                        cordX[playerX] = '#';
+                        System.out.println(cordX);
+                    }
+                    else
+                    {
+                        for (int index = 0; index < 10; index++)
+                        {
+                            cordX[index] = '-';
+                        }
+                        System.out.println(cordX);
+                    }
+                }
+            }
+            if(s.equals("a"))
+            {
+                playerX--;
+                for(int i = 0; i < cordY; i++)
+                {
+                    if(playerY == i)
+                    {
+                        cordX[playerX] = '#';
+                        System.out.println(cordX);
+                    }
+                    else
+                    {
+                        for (int index = 0; index < 10; index++)
+                        {
+                            cordX[index] = '-';
+                        }
+                        System.out.println(cordX);
+                    }
+                }
+            }
+            if(s.equals("s"))
+            {
+                playerY++;
+                for(int i = 0; i < cordY; i++)
+                {
+                    if(playerY == i)
+                    {
+                        cordX[playerX] = '#';
+                        System.out.println(cordX);
+                    }
+                    else
+                    {
+                        for (int index = 0; index < 10; index++)
+                        {
+                            cordX[index] = '-';
+                        }
+                        System.out.println(cordX);
+                    }
+                }
+            }
+            if(s.equals("w"))
+            {
+                playerY--;
+                for(int i = 0; i < cordY; i++)
+                {
+                    if(playerY == i)
+                    {
+                        cordX[playerX] = '#';
+                        System.out.println(cordX);
+                    }
+                    else
+                    {
+                        for (int index = 0; index < 10; index++)
+                        {
+                            cordX[index] = '-';
+                        }
+                        System.out.println(cordX);
+                    }
+                }
+            }
+            if(s.equals("exit"))
+            {
+                break;
+            }
+        }
+    }
+}
